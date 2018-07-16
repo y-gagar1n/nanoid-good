19b8ea9ae6d8fdf4fb13e61aaad7160da174a0f5
y-gagar1n <y.timofeyev@gmail.com>
2018-07-16 22:49:27 +0300

    fix imports

---
+++ test/hasProfanity.test.js
@@ -0,0 +1,13 @@
+var hasProfanity = require('../hasProfanity');
+
+it('returns false on innocent id', function() {
+    expect(hasProfanity("qwerty")).toEqual(false);
+});
+
+it('returns true on bad word', function() {
+    expect(hasProfanity("aSs")).toEqual(true);
+});
+
+it('returns true on bad word in between of larger word', function() {
+    expect(hasProfanity("abcaSsqwe")).toEqual(true);
+})
--- wrapper.js
+++ wrapper.js
@@ -1,14 +1,11 @@
-var Filter = require('bad-words');
-var en = require('naughty-words/en.json');
+var hasProfanity = require("./hasProfanity")
 
 
 module.exports = function (fn) {
     return function () {
-        var filter = new Filter();
-
         while (true) {
             var id = fn.apply(this, arguments);
-            if (!filter.isProfane(id)) return id;
+            if (!hasProfanity(id)) return id;
         }
     };
 };
