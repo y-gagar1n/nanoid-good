3c610c434160e2ddc76aad582c424c9b582711fe
y-gagar1n <y.timofeyev@gmail.com>
2018-07-16 23:53:16 +0300

    use all languages

--- hasProfanity.js
+++ hasProfanity.js
@@ -1,19 +1,23 @@
-var en = require("naughty-words/en.json");
-var de = require("naughty-words/de.json");
+var langs = ["en", "ar", "cs", "da", "de", "eo", 
+             "es", "fa", "fi", "fr", "hi", "hu", 
+             "it", "ja", "ko", "nl", "no", "pl", 
+             "pt", "ru", "sv", "th", "tlh", "tr", 
+             "zh"];
+// var en = require("naughty-words/en.json");
+// var de = require("naughty-words/de.json");
 
-var list = [en, de];
+var list = langs.map(lang => require("naughty-words/" + lang + ".json"));
 
 function checkByList(input, bad_words) {
-    for(var i = 0; i < bad_words.length; i++) {
-        if(new RegExp(bad_words[i], "gi").test(input)) return true;
+    for (var i = 0; i < bad_words.length; i++) {
+        if (new RegExp(bad_words[i], "gi").test(input)) return true;
     }
     return false;
 }
 
-module.exports = function(input) { 
-    for(var i =0; i < list.length; i++)
-    {
-        if(checkByList(input, list[i])) return true;
+module.exports = function (input) {
+    for (var i = 0; i < list.length; i++) {
+        if (checkByList(input, list[i])) return true;
     }
     return false;
 };
