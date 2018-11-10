b0e8d603871c71c3f263fee4af57e321390aaabb
y-gagar1n <y.timofeyev@gmail.com>
2018-11-10 23:33:27 +0300

    fix async wrapper (so that it actually works)

--- async/wrapper.js
+++ async/wrapper.js
@@ -4,22 +4,19 @@ var wrapper = function (fn) {
     return function (locales) {
         return function () {
             var outerArgs = arguments;
-            var result = new Promise(function(resolve, reject) {
-                try {
-                fn.apply(this, outerArgs)
+            return new Promise(function(resolve, reject) {
+                var retry = function() { fn.apply(this, outerArgs)
                     .catch(reject)
                     .then(function(id) {
                         if (!hasProfanity(locales)(id)) 
+                        {
                             resolve(id);
-                        else
-                            return result;
+                        }
+                        else retry();
                     });
-                }
-                catch(err) {
-                    reject(err);
-                }
+                };
+                retry();
             });
-            return result;
         }
     };
 };
